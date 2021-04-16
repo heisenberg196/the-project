977196c8e05125cbf23aa4d4f21199fbdd87bfbd
Jatin Popli <jatinpopli196@gmail.com>
2021-04-16 16:52:01 +0530

    home done

--- project/models.py
+++ project/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import User
-from django.dispatch import receiver 
-from django.db.models.signals import post_save 
+
 
 # Create your models here.
 class Project(models.Model):
@@ -18,15 +16,3 @@ def __str__(self):
 
 
 
-class Profile(models.Model):   
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	projects = models.ManyToManyField(Project)
-
-	@receiver(post_save, sender=User) #add this
-	def create_user_profile(sender, instance, created, **kwargs):
-		if created:
-			Profile.objects.create(user=instance)
-
-	@receiver(post_save, sender=User) #add this
-	def save_user_profile(sender, instance, **kwargs):
-		instance.profile.save()
