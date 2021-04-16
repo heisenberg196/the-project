b22c707b9058f4d330cffa83a5e465049cac7e50
Jatin Popli <jatinpopli196@gmail.com>
2021-04-16 13:16:20 +0530

    home page

--- colab_website/urls.py
+++ colab_website/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib import admin
+
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
--- project/models.py
+++ project/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.dispatch import receiver 
+from django.db.models.signals import post_save 
 
 # Create your models here.
 class Project(models.Model):
     project_name = models.CharField(max_length=90, verbose_name='Project Name')
+    # project_cateogory = 
+    
     image = models.FileField(upload_to='images/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     tech_used = models.TextField()
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
-
 def __str__(self):
     return "{title} by {creator}".format(title=self.project_name, creator=self.creator.first_name) 
+
+
+
+class Profile(models.Model):   
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	projects = models.ManyToManyField(Project)
+
+	@receiver(post_save, sender=User) #add this
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User) #add this
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
