1c075ea5cdc6fc92ac7613212252e593c4e61b2e
Jatin Popli <jatinpopli196@gmail.com>
2021-04-21 13:17:32 +0530

    commiting for pulling changes

--- project/models.py
+++ project/models.py
@@ -4,8 +4,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
+    # Project_Category = (
+    #     ('webdev', 'Web Development'),
+    #     ('anddev', 'Android Development'),
+    #     ('ml', 'Machine Learning'),
+    #     ('gd', 'Graphic'),
+    # )
     project_name = models.CharField(max_length=90, verbose_name='Project Name')
-    # project_cateogory = 
+    # rating = models.CharField(verbose_name="Skill Rating", max_length=1, choices=Skill_Rating)
+    description = models.CharField(max_length=320, verbose_name='Project Description')
     
     image = models.FileField(upload_to='images/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
